c560944194452005ef47151777018ff3a68d131f
Anselmo Ortega <lfkanselmo@gmail.com>
2023-11-15 11:28:05 -0500

    update

--- Spring Boot/mini-market/src/main/java/co/com/cesde/minimarket/persistence/mapper/CategoryMapper.java
+++ Spring Boot/mini-market/src/main/java/co/com/cesde/minimarket/persistence/mapper/CategoryMapper.java
@@ -1,7 +1,22 @@
 package co.com.cesde.minimarket.persistence.mapper;
 
+import co.com.cesde.minimarket.domain.Category;
+import co.com.cesde.minimarket.persistence.entity.Categoria;
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring")
 public interface CategoryMapper {
+    @Mappings({
+            @Mapping(source = "idCategoria", target = "categoryId"),
+            @Mapping(source = "nombreCategoria", target = "categoryName"),
+            @Mapping(source = "estado", target = "status")
+    })
+    Category toCategory(Categoria categoria);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "productos", ignore = true)
+    Categoria toCategoria(Category category);
 }
--- Spring Boot/mini-market/src/main/java/co/com/cesde/minimarket/persistence/mapper/ProductMapper.java
+++ Spring Boot/mini-market/src/main/java/co/com/cesde/minimarket/persistence/mapper/ProductMapper.java
@@ -1,4 +1,31 @@
 package co.com.cesde.minimarket.persistence.mapper;
 
+import co.com.cesde.minimarket.domain.Product;
+import co.com.cesde.minimarket.persistence.entity.Producto;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {CategoryMapper.class})
 public interface ProductMapper {
+
+    @Mappings({
+            @Mapping(source = "idProducto", target = "productId"),
+            @Mapping(source = "nombreProducto", target = "productName"),
+            @Mapping(source = "idCategoria", target = "categoryId"),
+            @Mapping(source = "precioVenta", target = "price"),
+            @Mapping(source = "cantidadStock", target = "stock"),
+            @Mapping(source = "estado", target = "status"),
+            @Mapping(source = "categoria", target = "category")
+    })
+    Product toProduct(Producto producto);
+    List<Product> toProducts(List<Producto> productos);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "codigoBarras", ignore = true)
+    Producto toProducto(Product product);
+    List<Producto> toProductos(List<Product> products);
 }
